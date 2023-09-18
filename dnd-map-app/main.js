ed50c4c3b3186f963cda21bb8415e9bcc2fb279f
Alex Damjanovski <alexdamjano@gmail.com>
2023-09-17 20:17:30 -0400

    getting the prinnal map in as a static image

--- dnd-map-app/main.js
+++ dnd-map-app/main.js
@@ -1,17 +1,36 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import "./style.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import ImageLayer from "ol/layer/Image.js";
+import OSM from "ol/source/OSM";
+import Projection from "ol/proj/Projection.js";
+import Static from "ol/source/ImageStatic.js";
+import { getCenter } from "ol/extent.js";
+const extent = [0, 0, 2100, 1200];
+const projection = new Projection({
+	code: "satmap-image",
+	units: "pixels",
+	extent: extent,
+});
 
 const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+	target: "map",
+	layers: [
+		new TileLayer({
+			source: new OSM(),
+		}),
+		new ImageLayer({
+			source: new Static({
+				url: "http://127.0.0.1:5500/sat_map.png",
+				projection: projection,
+				imageExtent: extent,
+			}),
+		}),
+	],
+	target: "map",
+	view: new View({
+		projection: projection,
+		center: getCenter(extent),
+		zoom: 2.58,
+	}),
 });
